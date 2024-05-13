ebda9d1f5758d62c26c990bc3daf29a806c5f1a5
jgmbl <joannagembel@gmail.com>
2024-05-13 17:52:27 +0200

    TDL-17 add NamesRepository

---
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesRepository.java
@@ -0,0 +1,6 @@
+package pl.jgmbl.to_do_list_backend.names;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NamesRepository extends JpaRepository<Names, Integer> {
+}
