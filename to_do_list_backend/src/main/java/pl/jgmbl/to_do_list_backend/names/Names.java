8fca7dba6ca77b4de6be4624c44bcc253d238658
jgmbl <joannagembel@gmail.com>
2024-05-13 17:51:06 +0200

    TDL-17 create Names Entity

---
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/Names.java
@@ -0,0 +1,29 @@
+package pl.jgmbl.to_do_list_backend.names;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Names {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Names() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
