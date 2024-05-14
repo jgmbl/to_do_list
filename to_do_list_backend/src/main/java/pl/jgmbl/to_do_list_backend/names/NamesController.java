2ba5db5608ee5245f93d8c153c5da1099c3af591
jgmbl <joannagembel@gmail.com>
2024-05-14 12:54:18 +0200

    TDL-19 add displayAllNames method

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesController.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesController.java
@@ -1,7 +1,22 @@
 package pl.jgmbl.to_do_list_backend.names;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class NamesController {
+
+    private final NamesService namesService;
+
+    public NamesController(NamesService namesService) {
+        this.namesService = namesService;
+    }
+
+    @GetMapping("/names")
+    public ResponseEntity<Iterable<Names>> displayAllNames() {
+        Iterable<Names> allNames = namesService.getAllNames();
+
+        return ResponseEntity.ok(allNames);
+    }
 }
