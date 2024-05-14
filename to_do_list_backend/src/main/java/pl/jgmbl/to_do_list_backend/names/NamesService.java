cd6a5b6458ff47e5a6429b94553e21f6fa6d96d4
jgmbl <joannagembel@gmail.com>
2024-05-14 12:53:15 +0200

    TDL-19 add getAllNames method

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesService.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesService.java
@@ -4,4 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NamesService {
+    private final NamesRepository namesRepository;
+
+    public NamesService(NamesRepository namesRepository) {
+        this.namesRepository = namesRepository;
+    }
+
+    public Iterable<Names> getAllNames () {
+        return namesRepository.findAll();
+    }
 }
