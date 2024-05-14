ad642d228fecb7bcf3e6714832a6f4c740eb87e9
jgmbl <joannagembel@gmail.com>
2024-05-14 12:41:26 +0200

    TDL-14 add NamesController and NamesService

---
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesController.java
@@ -0,0 +1,7 @@
+package pl.jgmbl.to_do_list_backend.names;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NamesController {
+}
---
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesService.java
@@ -0,0 +1,7 @@
+package pl.jgmbl.to_do_list_backend.names;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NamesService {
+}
