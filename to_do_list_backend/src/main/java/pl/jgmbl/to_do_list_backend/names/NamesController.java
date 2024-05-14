9abd84511521c75d2400b1f0b63a9c4dd4fe9608
jgmbl <joannagembel@gmail.com>
2024-05-14 13:20:26 +0200

    TDL-19 add displayNameById method

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesController.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/names/NamesController.java
@@ -2,7 +2,11 @@ package pl.jgmbl.to_do_list_backend.names;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class NamesController {
@@ -18,5 +22,14 @@ public class NamesController {
         Iterable<Names> allNames = namesService.getAllNames();
 
         return ResponseEntity.ok(allNames);
+    }
+
+    @GetMapping("/name/{id}")
+    public ResponseEntity<Names> displayNameById (@RequestParam(name = "id") Integer id) {
+        Optional<Names> nameById = namesService.getNameById(id);
+
+        return nameById
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
