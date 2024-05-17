3ed5e95330ff8e6582a65096bd313fcc152bb46c
jgmbl <joannagembel@gmail.com>
2024-05-17 13:43:09 +0200

    TDL-10 add findByNamesId method to TasksRepository

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksRepository.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksRepository.java
@@ -3,4 +3,5 @@ package pl.jgmbl.to_do_list_backend.tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TasksRepository extends JpaRepository<Tasks, Integer> {
+    Iterable<Tasks> findByNamesId(Integer nameId);
 }
