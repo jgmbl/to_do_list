1b45952e2518ebf87254e25ed089afbbf84c2025
jgmbl <joannagembel@gmail.com>
2024-05-17 13:10:28 +0200

    TDL-10 add getAllTasks method to TasksController

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksController.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksController.java
@@ -1,7 +1,21 @@
 package pl.jgmbl.to_do_list_backend.tasks;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TasksController {
+    private final TasksService tasksService;
+
+    public TasksController(TasksService tasksService) {
+        this.tasksService = tasksService;
+    }
+
+    @GetMapping("/tasks")
+    public ResponseEntity<Iterable<Tasks>> getAllTasks() {
+        Iterable<Tasks> allTasks = tasksService.getAllTasks();
+
+        return ResponseEntity.ok(allTasks);
+    }
 }
