52a6fc58f4b9e350a2c1ee8c34fd38142fe59e54
jgmbl <joannagembel@gmail.com>
2024-05-17 13:44:22 +0200

    TDL-10 add getTasksByNameId to TasksController

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksController.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksController.java
@@ -2,6 +2,7 @@ package pl.jgmbl.to_do_list_backend.tasks;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,16 @@ public class TasksController {
         Iterable<Tasks> allTasks = tasksService.getAllTasks();
 
         return ResponseEntity.ok(allTasks);
+    }
+
+    @GetMapping("/tasks/{nameId}")
+    public ResponseEntity<Iterable<Tasks>> getTasksByNameId(@PathVariable Integer nameId) {
+        Iterable<Tasks> tasksByNameId = tasksService.getTasksByNameId(nameId);
+
+        if (tasksByNameId == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(tasksByNameId);
     }
 }
