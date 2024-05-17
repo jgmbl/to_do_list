9e98ddf37f76407870d71363170076e0fe851b53
jgmbl <joannagembel@gmail.com>
2024-05-17 13:45:04 +0200

    TDL-10 add getTasksByNameId method to TasksService

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksService.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksService.java
@@ -1,16 +1,34 @@
 package pl.jgmbl.to_do_list_backend.tasks;
 
 import org.springframework.stereotype.Service;
+import pl.jgmbl.to_do_list_backend.names.Names;
+import pl.jgmbl.to_do_list_backend.names.NamesRepository;
+
+import java.util.Optional;
 
 @Service
 public class TasksService {
     private final TasksRepository tasksRepository;
+    private final NamesRepository namesRepository;
 
-    public TasksService(TasksRepository tasksRepository) {
+    public TasksService(TasksRepository tasksRepository, NamesRepository namesRepository) {
         this.tasksRepository = tasksRepository;
+        this.namesRepository = namesRepository;
     }
 
     protected Iterable<Tasks> getAllTasks() {
         return tasksRepository.findAll();
+    }
+
+    protected Iterable<Tasks> getTasksByNameId(Integer nameId) {
+        Optional<Names> names = doesNameIdExists(nameId);
+        if (names.isEmpty()) {
+            return null;
+        }
+        return tasksRepository.findByNamesId(nameId);
+    }
+
+    private Optional<Names> doesNameIdExists(Integer nameId) {
+        return namesRepository.findById(nameId);
     }
 }
