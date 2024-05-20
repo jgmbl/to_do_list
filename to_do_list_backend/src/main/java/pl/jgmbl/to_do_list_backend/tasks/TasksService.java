bf39dd6374fea653c4aec218ef5f01b3cccaee89
jgmbl <joannagembel@gmail.com>
2024-05-20 11:47:40 +0200

    TDL-9 delete unused methods in TasksService

--- to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksService.java
+++ to_do_list_backend/src/main/java/pl/jgmbl/to_do_list_backend/tasks/TasksService.java
@@ -23,7 +23,7 @@ public class TasksService {
     }
 
     protected Iterable<Tasks> getTasksByNameId(Integer nameId) {
-        Optional<Names> names = doesNameIdExists(nameId);
+        Optional<Names> names = namesRepository.findById(nameId);
         if (names.isEmpty()) {
             return null;
         }
@@ -52,9 +52,5 @@ public class TasksService {
             tasksRepository.deleteById(taskId);
             return true;
         }
-    }
-
-    private Optional<Names> doesNameIdExists(Integer nameId) {
-        return namesRepository.findById(nameId);
     }
 }
