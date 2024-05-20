e13842c0c0f795422ad0e62c3f4aa51e6ee3672a
jgmbl <joannagembel@gmail.com>
2024-05-20 15:37:02 +0200

    TDL-24 add getAllNames method test

--- to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/names/NamesServiceUnitTest.java
+++ to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/names/NamesServiceUnitTest.java
@@ -1,10 +1,18 @@
 package pl.jgmbl.to_do_list_backend.names;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class NamesServiceUnitTest {
@@ -16,6 +24,23 @@ class NamesServiceUnitTest {
 
     @Test
     void getAllNames() {
+        Names names = new Names(1, "XYZ");
+        Names names1 = new Names(2, "ABC");
+
+        when(namesRepository.findAll()).thenReturn(Arrays.asList(names, names1));
+        Iterable<Names> allNames = namesService.getAllNames();
+
+        List<Names> allNamesList = new ArrayList<>();
+        for (Names name : allNames) {
+            allNamesList.add(name);
+        }
+
+        Assertions.assertEquals(allNamesList.size(), 2);
+        Assertions.assertEquals(allNamesList.get(0).getId(), 1);
+        Assertions.assertEquals(allNamesList.get(0).getName(), "XYZ");
+        Assertions.assertEquals(allNamesList.get(1).getId(), 2);
+        Assertions.assertEquals(allNamesList.get(1).getName(), "ABC");
+
     }
 
     @Test
