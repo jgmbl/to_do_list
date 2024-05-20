d6a3438d70b760097ae59a959d4d48cda3d06aa5
jgmbl <joannagembel@gmail.com>
2024-05-20 13:19:54 +0200

    TDL-22 add GetAllNames test

--- to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/names/NamesControllerUnitTest.java
+++ to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/names/NamesControllerUnitTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(NamesController.class)
@@ -15,11 +23,19 @@ class NamesControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
+    @MockBean
     private NamesService namesService;
 
     @Test
-    void getAllNames() {
+    void getAllNames() throws Exception {
+        when(namesService.getAllNames()).thenReturn(Collections.singletonList(new Names()));
+
+        mockMvc.perform(get("/names"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$").isArray());
     }
 
     @Test
