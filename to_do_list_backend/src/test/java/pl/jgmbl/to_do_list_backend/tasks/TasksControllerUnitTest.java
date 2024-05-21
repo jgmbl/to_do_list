d943a727661f92bf0900e835079686d8a7ca8678
jgmbl <joannagembel@gmail.com>
2024-05-21 12:44:19 +0200

    TDL-23 set up TasksControllerUnitTests

---
+++ to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/tasks/TasksControllerUnitTest.java
@@ -0,0 +1,44 @@
+package pl.jgmbl.to_do_list_backend.tasks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TasksController.class)
+class TasksControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private TasksService tasksService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void getTasksByNameId() {
+    }
+
+    @Test
+    void postTask() {
+    }
+
+    @Test
+    void patchTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
