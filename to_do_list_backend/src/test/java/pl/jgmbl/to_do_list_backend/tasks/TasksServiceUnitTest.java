ec36c1b420bd99eb5eead76e9d63b7a1603b228b
jgmbl <joannagembel@gmail.com>
2024-05-22 12:49:05 +0200

    TDL-25 add getTasksByNameId test

--- to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/tasks/TasksServiceUnitTest.java
+++ to_do_list_backend/src/test/java/pl/jgmbl/to_do_list_backend/tasks/TasksServiceUnitTest.java
@@ -7,11 +7,16 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pl.jgmbl.to_do_list_backend.names.Names;
+import pl.jgmbl.to_do_list_backend.names.NamesRepository;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
+import static java.util.Optional.empty;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -19,6 +24,8 @@ import static org.mockito.Mockito.when;
 class TasksServiceUnitTest {
     @Mock
     TasksRepository tasksRepository;
+    @Mock
+    NamesRepository namesRepository;
     @InjectMocks
     TasksService tasksService;
 
@@ -32,7 +39,7 @@ class TasksServiceUnitTest {
         when(tasksRepository.findAll()).thenReturn(Arrays.asList(tasks, tasks1));
         Iterable<Tasks> allTasks = tasksService.getAllTasks();
 
-        ArrayList<Tasks> allTasksList = new ArrayList<>();
+        List<Tasks> allTasksList = new ArrayList<>();
         for (Tasks task : allTasks) {
             allTasksList.add(task);
         }
@@ -48,6 +55,35 @@ class TasksServiceUnitTest {
 
     @Test
     void getTasksByNameId() {
+        Names names = new Names(1, "ABC");
+        Tasks tasks = new Tasks(1, names, "XYZ");
+        Tasks tasks1 = new Tasks(2, names, "TEST");
+
+        when(tasksRepository.findByNamesId(1)).thenReturn(Arrays.asList(tasks, tasks1));
+        when(namesRepository.findById(1)).thenReturn(Optional.of(names));
+        Iterable<Tasks> tasksByNameId = tasksService.getTasksByNameId(1);
+        Optional<Names> namesRepositoryById = namesRepository.findById(1);
+        Names nameFromRepository = namesRepositoryById.orElseGet(null);
+
+        List<Tasks> tasksByNameIdList = new ArrayList<>();
+        for (Tasks task : tasksByNameId) {
+            tasksByNameIdList.add(task);
+        }
+
+        assertNotEquals(tasksByNameId, empty());
+        assertNotEquals(tasksByNameId, null);
+        assertEquals(nameFromRepository.getId(), 1);
+        assertEquals(nameFromRepository.getName(), "ABC");
+        assertEquals(tasksByNameIdList.get(0).getId(), 1);
+        assertEquals(tasksByNameIdList.get(0).getNames().getName(), "ABC");
+        assertEquals(tasksByNameIdList.get(0).getNames().getId(), 1);
+        assertEquals(tasksByNameIdList.get(0).getContent(), "XYZ");
+        assertNotEquals(tasksByNameIdList.get(0).getContent(), "TEST");
+        assertEquals(tasksByNameIdList.get(1).getId(), 2);
+        assertEquals(tasksByNameIdList.get(1).getNames().getName(), "ABC");
+        assertEquals(tasksByNameIdList.get(1).getNames().getId(), 1);
+        assertEquals(tasksByNameIdList.get(1).getContent(), "TEST");
+
     }
 
     @Test
