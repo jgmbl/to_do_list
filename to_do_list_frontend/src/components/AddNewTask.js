af995b4ea9af6ddcdc0cf4fbe15b9f82d100652e
jgmbl <joannagembel@gmail.com>
2024-05-23 14:05:48 +0200

    TDL-26 add spacing between components in components/AddNewTask.js

--- to_do_list_frontend/src/components/AddNewTask.js
+++ to_do_list_frontend/src/components/AddNewTask.js
@@ -1,24 +1,28 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import NamesDropDownMenu from './NamesDropDownMenu';
+import { Container, Paper, Stack } from '@mui/material';
 
 export default function AddNewTask() {
   return (
-    <Box
+    <Container
       component="form"
       sx={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        '& > :not(style)': { m: 1, width: '25ch' },
+        '& > :not(style)': { m: 1, width: '25ch'},
       }}
       noValidate
       autoComplete="off"
     >
-       <NamesDropDownMenu sx={{justifyContent: 'center', width: '100%'}}/>
-      <TextField id="name" label="Name" variant="outlined" />
-      <TextField id="task" label="Task" variant="outlined" />
-    </Box>
+        <Paper elevation={3} sx={{ p: 3 }}>
+            <Stack spacing={2}>
+              <NamesDropDownMenu sx={{justifyContent: 'center'}}/>
+            <TextField id="name" label="Name" variant="outlined" />
+            <TextField id="task" label="Task" variant="outlined" />
+            </Stack>
+      </Paper>
+    </Container>
   );
 }
