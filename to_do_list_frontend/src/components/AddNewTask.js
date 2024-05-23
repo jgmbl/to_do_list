fa435b2e3c2a623e20e244295954e167f2e76300
jgmbl <joannagembel@gmail.com>
2024-05-23 14:34:32 +0200

    TDL-26 add button in components/AddNewTask.js

--- to_do_list_frontend/src/components/AddNewTask.js
+++ to_do_list_frontend/src/components/AddNewTask.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import NamesDropDownMenu from './NamesDropDownMenu';
+import ButtonMainMenu from './ButtonMainMenu';
 import { Container, Paper, Stack } from '@mui/material';
 
 export default function AddNewTask() {
@@ -17,10 +18,16 @@ export default function AddNewTask() {
       autoComplete="off"
     >
         <Paper elevation={3} sx={{ p: 3 }}>
+            <h1 style={{color: "#338BA8"}}>Welcome to to-do list app!</h1>
+            <p style={{color: "#707070"}}>Create to-do lists and assign tasks to users.</p>
             <Stack spacing={2}>
+            <b style={{color: "#707070"}}>Select a name from the list...</b>
               <NamesDropDownMenu sx={{justifyContent: 'center'}}/>
+              <b style={{color: "#707070"}}>...or type your name:</b>
             <TextField id="name" label="Name" variant="outlined" />
+            <b style={{color: "#707070"}}>Enter the task:</b>
             <TextField id="task" label="Task" variant="outlined" />
+            <ButtonMainMenu/>
             </Stack>
       </Paper>
     </Container>
