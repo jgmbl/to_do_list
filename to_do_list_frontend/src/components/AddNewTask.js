596d1f4ce31edae9febb7815519062d2ed15e9a8
jgmbl <joannagembel@gmail.com>
2024-05-23 13:40:24 +0200

    TDL-26 add new drop-down menu to components/AddNewTask.js

--- to_do_list_frontend/src/components/AddNewTask.js
+++ to_do_list_frontend/src/components/AddNewTask.js
@@ -1,17 +1,22 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import NamesDropDownMenu from './NamesDropDownMenu';
 
 export default function AddNewTask() {
   return (
     <Box
       component="form"
       sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
         '& > :not(style)': { m: 1, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
     >
+       <NamesDropDownMenu sx={{justifyContent: 'center', width: '100%'}}/>
       <TextField id="name" label="Name" variant="outlined" />
       <TextField id="task" label="Task" variant="outlined" />
     </Box>
