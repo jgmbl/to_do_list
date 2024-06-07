1dcf47e314e7cf89dbd4813452d463b39ad84c38
jgmbl <joannagembel@gmail.com>
2024-06-07 14:11:04 +0200

    TDL-26 temp save changes in files

--- to_do_list_frontend/src/components/Appbar.js
+++ to_do_list_frontend/src/components/Appbar.js
@@ -9,7 +9,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function Appbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" sx={{ backgroundColor: "#338BA8" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -20,8 +20,8 @@ export default function Appbar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            To-Do List
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+            TO-DO LIST
           </Typography>
         </Toolbar>
       </AppBar>
