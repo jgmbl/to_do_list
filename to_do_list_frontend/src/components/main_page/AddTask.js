b2b581623b0211cbfd2b68aeb7b7eafac415477a
jgmbl <joannagembel@gmail.com>
2024-06-14 13:05:26 +0200

    TDL-26 fix the bug in AddTask.js

--- to_do_list_frontend/src/components/main_page/AddTask.js
+++ to_do_list_frontend/src/components/main_page/AddTask.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import { getIdByName } from "./GetIdByName";
 
-export const addTask = async (id_name, content) => {
-    if (!task || typeof task !== 'string' || task.trim === '') {
+export const addTask = async (names, content) => {
+    if (!content || typeof content !== 'string' || content.trim() === '') {
         toast.error("Enter a task", {
             position: 'top-right',
             closeOnClick:true
@@ -11,17 +12,24 @@ export const addTask = async (id_name, content) => {
         return;
     }
 
+    const id = await getIdByName(names);
 
     const newTask = {
-        "names": {
-            "id": id_name,
+        names: {
+            id
         },
-        "content": content,
+        content
     };
 
     try {
-        const response = await axios.post('/task', { newTask });
+        const response = await axios.post('/tasks', newTask);
+        console.log("Task is added to database: ", response.data);
+        return response.data;
     } catch (error) {
+        toast.error("Task cannot be added: " + error.message, {
+            position: 'top-right',
+            closeOnClick: true
+        });
         console.error("Task cannot be added: ", error);
     }
 }
