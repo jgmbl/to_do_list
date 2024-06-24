374f44ff8584b008c1c03ee5e09fd2db105ddf4e
jgmbl <joannagembel@gmail.com>
2024-06-24 11:28:39 +0200

    TDL-26 add displaying list of tasks and names in ListOfTasks.js

--- to_do_list_frontend/src/components/main_page/ListOfTasks.js
+++ to_do_list_frontend/src/components/main_page/ListOfTasks.js
@@ -1,39 +1,38 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Container, Paper } from '@mui/material';
+import { getListOfNamesAndTasks } from './getListOfNamesAndTasks';
 
 const columns = [
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   width: 90,
-  // },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
-  },
+  { field: 'names', headerName: 'Name', width: 150 },
+  { field: 'tasks', headerName: 'Task', width: 150 },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
+// const rows = [
+//   { id: getListOfNamesAndTasks.id, names: getListOfNamesAndTasks.name, tasks: getListOfNamesAndTasks.tasks},
+// ];
 
 export default function DataTable() {
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dataTasks = await getListOfNamesAndTasks();
+
+        if (dataTasks) {
+          setRows(dataTasks);
+        }
+      } catch (e) {
+        console.error("Error fetching tasks: ", e);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <Container
       sx={{
@@ -46,6 +45,7 @@ export default function DataTable() {
       autoComplete="off"
     >
       <Paper elevation={3} sx={{ p: 3 }}>
+      <h2 style={{color: "#707070"}}>List of tasks: </h2>
         <div style={{ height: 400, width: '100%' }}>
           <DataGrid
             rows={rows}
